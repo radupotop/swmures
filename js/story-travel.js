b7f812d72872da57b8ab46c4a7cf1af7d5cd8782
Radu Potop <radu.potop@wooptoo.com>
2013-05-25 16:17:20 +0300

    fix selectedTags

--- js/story-travel.js
+++ js/story-travel.js
@@ -64,9 +64,10 @@ google.maps.event.addDomListener(window, 'load', StoryTravel.Init);
  */
 function GetTags() {
     
-    var tags = [];
-    var selectedTags = [];
     var ret = {};
+    var availableTags = [];
+    
+    ret.selectedTags = [];
     
     /**
      * Get data & tags from JSON
@@ -77,17 +78,17 @@ function GetTags() {
 	    
 	    $.each(data.data, function(k,v) {
 		$.each(v.tags, function(k,v) {
-		    tags.push(v);
+		    availableTags.push(v);
 		});
 	    });
 	    
-	    $.unique(tags);
+	    $.unique(availableTags);
 	    
 	    appendTags();
 	    
 	});
     }
-
+    
     getTags();
 
 
@@ -96,8 +97,8 @@ function GetTags() {
      */
     function appendTags() {
 	
-	$.each(tags, function(k, tag) {
-	    $('#DataTags').append('<strong class="label">'+tag+'</strong> <a href="#" class="btn btn-small" onclick="Tags.addTag('+tag+')">+</a> <a href="#" class="btn btn-small" onclick="Tags.rmTag('+tag+')">-</a><br>');
+	$.each(availableTags, function(k, tag) {
+	    $('#DataTags').append('<strong class="label">'+tag+'</strong> <a href="#" class="btn btn-small" onclick="Tags.addTag(\''+tag+'\')">+</a> <a href="#" class="btn btn-small" onclick="Tags.rmTag(\''+tag+'\')">-</a><br>');
 	});
 	
     }
@@ -107,20 +108,24 @@ function GetTags() {
      * Add a tag
      */
     ret.addTag = function(tag) {
-	selectedTags.push(tag);
-	console.log(selectedTags);
-    }
+	ret.selectedTags.push(tag);
+	$.unique(ret.selectedTags);
+	console.log(ret.selectedTags);
+    };
     
     /**
      * Remove a tag
      */
     ret.rmTag = function(tag) {
-	//~selectedTags.find();
-    }
+	ret.selectedTags = ret.selectedTags.filter(function(t){
+	    return t != tag;
+	});
+	console.log(ret.selectedTags);
+    };
     
     return ret;
     
 };
 
 
-var Tags = GetTags(); 
+var Tags = GetTags();
